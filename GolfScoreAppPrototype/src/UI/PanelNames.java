0968ad01ed8197aefa77c399486f1470090f9adc
Daren Cheng <2012DCheng@gmail.com>
2015-12-03 07:08:20 -0600

    Added PanelNames list of defined String constants to standardize show(String panelName) method in UI,

---
+++ GolfScoreAppPrototype/src/UI/PanelNames.java
@@ -0,0 +1,15 @@
+package UI;
+
+public class PanelNames {
+	//defined sting constants
+	public static final String HOME = "Home";
+	public static final String CREATE_COURSE = "Create Course";
+	public static final String START_ROUND = "Start Round";
+	public static final String START_HOLE = "Start Hole";
+	public static final String START_SHOT = "Start Shot";
+	public static final String END_ROUND = "End Round";
+	public static final String END_HOLE = "End Hole";
+	public static final String END_SHOT = "End Shot";
+	public static final String UPLOAD_DATA = "Upload Data";
+	public static final String VIEW_HISTORY = "View History";
+}
